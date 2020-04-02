78fd37220dc44e10b61e00e491f70a32e1516042
Charles Odili <chaluwa@gmail.com>
2020-04-02 19:48:42 +0100

    trim down tests and lighthouse audits threshold

--- ch-2/estimator.test.js
+++ ch-2/estimator.test.js
@@ -1,5 +1,5 @@
 /**
  * @jest-environment node
  */
-import{onCovid19}from"on-covid-19";import estimator from"../../src/estimator.js";import{getImpactDataStructure,valueOnFields,mockEstimationFor}from"../test-utils.js";const cases=[["days","ch-2"],["weeks","ch-2"],["months","ch-2"]];describe("on-covid-19 >> Challenge-1",()=>{test.each(cases)("estimate current and projected infections, in %s",async(t,e)=>{const a=await mockEstimationFor(t),{data:o,estimate:s}=a.data,{periodType:c,timeToElapse:i}=o,r=await onCovid19(o,estimator).estimateImpactAfter(i)[c]();expect(r).toBeTruthy(),expect(r).toMatchObject(getImpactDataStructure(e)),valueOnFields(r,s,e).forEach(([t,e])=>{expect(t).toBe(e)})})});
+import{onCovid19}from"on-covid-19";import estimator from"../../src/estimator.js";import{getImpactDataStructure,valueOnFields,mockEstimationFor}from"../test-utils.js";const cases=[["days","ch-2"],["weeks","ch-2"],["months","ch-2"]];describe("on-covid-19 >> Challenge-2",()=>{test.each(cases)("estimate current and projected infections, in %s",async(t,e)=>{const a=await mockEstimationFor(t),{data:o,estimate:s}=a.data,{periodType:c,timeToElapse:i}=o,r=await onCovid19(o,estimator).estimateImpactAfter(i)[c]();expect(r).toBeTruthy(),expect(r).toMatchObject(getImpactDataStructure(e)),valueOnFields(r,s,e).forEach(([t,e])=>{expect(t).toBe(e)})})});
  
